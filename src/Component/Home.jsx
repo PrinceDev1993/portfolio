de462aa62f02e056cfc795e73157e26b986a55a4
Princewill Onyebuchi <evangelize4me@gmail.com>
2024-02-29 07:23:25 +0100

    third  commit

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -20,13 +20,34 @@ const Home = () => {
                         </span>
                     </Link>
 
-                    <button>
-                    <a href="https://github.com/PrinceDev1993" className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
-                        GitHub Page 
-                        <span className='group-hover:rotate-90 duration-300'>
-                            <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
-                        </span>
-                    </a>
+                    <button className=''>
+                        <a href="https://github.com/PrinceDev1993" className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                            GitHub Page 
+                            <span className='group-hover:rotate-90 duration-300'>
+                                <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
+                            </span>
+                        </a>
+                    </button>
+                </div>
+
+                <div className='flex justify-between'>
+                    <button className='lg:hidden'>
+                        <a href="https://github.com/PrinceDev1993" className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                            LinkedIn 
+                            <span className='group-hover:rotate-90 duration-300'>
+                                <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
+                            </span>
+                        </a>
+                    </button>
+
+                    <button className='lg:hidden'>
+                        <a href="./assets/PRINCEWILL CV.docx" download target='_blank'
+                        rel='noreferrer' className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                            Download CV 
+                            <span className='group-hover:rotate-90 duration-300'>
+                                <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
+                            </span>
+                        </a>
                     </button>
                 </div>
             </div>
