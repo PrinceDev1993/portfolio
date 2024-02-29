f184d9132a95ecf4e5f5a73d19235dea7fd0d18d
Princewill Onyebuchi <evangelize4me@gmail.com>
2024-02-29 07:25:21 +0100

    Fourth commit

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
 
                 <div className='flex justify-between'>
                     <button className='lg:hidden'>
-                        <a href="https://github.com/PrinceDev1993" className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                        <a href="https://www.linkedin.com/in/princewillonyebuchi/" className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                             LinkedIn 
                             <span className='group-hover:rotate-90 duration-300'>
                                 <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
